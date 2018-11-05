7e48448cdbc94f3660fb0ee1d85080b9fd60feb0
Stan Gilstrap <Stangil44@Gmail.com>
2018-11-05 14:05:55 -0500

    added ability to click url to map

--- public/js/index.js
+++ public/js/index.js
@@ -17,12 +17,16 @@ socket.on('newMessage', function (message) {
   jQuery('#messages').append(li);
 });
 
-// socket.emit('createMessage',{
-//   from: 'Jorge',
-//   text: 'Wassup'
-// }, function(data){
-//   console.log('recieved message', data);//acknowledment
-// });
+socket.on('newLocationMessage', function(message){
+  var li= jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My current Location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+
+});
 
 jQuery('#message-form').on('submit', function(e){
   e.preventDefault();
@@ -30,6 +34,22 @@ jQuery('#message-form').on('submit', function(e){
     from: 'User',
     text: jQuery('[name=message]').val()
   },function(){
+  });
+});
 
+var locationButton = jQuery('#send-location');
+
+locationButton.on('click', function(){
+  if(!navigator.geolocation){
+    return alert('Geolocation  not supported for your browser.');
+  }
+  navigator.geolocation.getCurrentPosition(function(position){
+    
+   socket.emit('createLocationMessage',{
+     latitude: position.coords.latitude,
+     longitude: position.coords.longitude
+   });
+  },function(){
+    alert('Unable to fetch location');
   });
 });
