a6bc1240629b7e18426f588c9f5c9ac5a5d0129b
Stan Gilstrap <Stangil44@Gmail.com>
2018-11-05 11:57:24 -0500

    add form for messages

--- public/js/index.js
+++ public/js/index.js
@@ -11,4 +11,25 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+// socket.emit('createMessage',{
+//   from: 'Jorge',
+//   text: 'Wassup'
+// }, function(data){
+//   console.log('recieved message', data);//acknowledment
+// });
+
+jQuery('#message-form').on('submit', function(e){
+  e.preventDefault();
+  socket.emit('createMessage',{
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  },function(){
+
+  });
 });
